8e11818cf56050a584b0258e4e287b9edc7fcbb9
PieterNatanael <pieternatanael@icloud.com>
2023-09-25 13:45:03 +0800

    Initial Commit

---
+++ TebakAngkasIOS/TebakAngkasIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TebakAngkasIOSApp.swift
+//  TebakAngkasIOS
+//
+//  Created by Pieter Yoshua Natanael on 25/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TebakAngkasIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
